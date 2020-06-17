78834cf35eeb0874e6fa071a4235e756c705185a
Omar-Abdelrady <omarabdelrady123@gmail.com>
2020-06-17 23:48:15 +0200

    Updates

---
+++ precache-manifest.6ac8b8a5a3595547e79aba720bba5615.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "623cc4f8e76f39e916cf5413c691f375",
+    "url": "/Maysan-For-Sale-Omar.dev/index.html"
+  },
+  {
+    "revision": "a0cc1465ab1c0103a34f",
+    "url": "/Maysan-For-Sale-Omar.dev/static/css/2.3ed69fa8.chunk.css"
+  },
+  {
+    "revision": "ada5068793dd51f3dc6a",
+    "url": "/Maysan-For-Sale-Omar.dev/static/css/main.45dd8616.chunk.css"
+  },
+  {
+    "revision": "a0cc1465ab1c0103a34f",
+    "url": "/Maysan-For-Sale-Omar.dev/static/js/2.f8ac0851.chunk.js"
+  },
+  {
+    "revision": "882dda980fb22bdfe71fa2cbeb9b28ff",
+    "url": "/Maysan-For-Sale-Omar.dev/static/js/2.f8ac0851.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ada5068793dd51f3dc6a",
+    "url": "/Maysan-For-Sale-Omar.dev/static/js/main.7d2889f7.chunk.js"
+  },
+  {
+    "revision": "62705dfa7181582226ea",
+    "url": "/Maysan-For-Sale-Omar.dev/static/js/runtime-main.687dd5e7.js"
+  },
+  {
+    "revision": "413f6f4e3a628e1e9c11b9931b5f2b1b",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/Firefox - Mac & Windows 1.413f6f4e.png"
+  },
+  {
+    "revision": "12fe0deacaf4b3ae5eecc20701b3272e",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/FontsFree-Net-din-next-arabic-bold.12fe0dea.ttf"
+  },
+  {
+    "revision": "b9824811f4eaf80005f4f6628833a2cb",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/Group 141.b9824811.png"
+  },
+  {
+    "revision": "b19c5da8c4caa4184dc4912697110b88",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/Group.b19c5da8.svg"
+  },
+  {
+    "revision": "89d159f97b3c83e84ea71283411606ab",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/HH.89d159f9.png"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "4ff64998a73076f329619e4b72b01cc3",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/bg-circle.4ff64998.svg"
+  },
+  {
+    "revision": "cd4ab2c8d2b440042ffc2a0f40c60c31",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/bg_elements.cd4ab2c8.jpg"
+  },
+  {
+    "revision": "4ec4aebb07b8584e9f731ca773f9f079",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/blue_circle.4ec4aebb.svg"
+  },
+  {
+    "revision": "eb6101015a9e5f639c1c302719044d57",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/bordered_logo.eb610101.svg"
+  },
+  {
+    "revision": "6e1de5c116dcf51c545323a3147942d5",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/comment_photo.6e1de5c1.png"
+  },
+  {
+    "revision": "404b7ba9877a4c2c065355d97c96c836",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/design_01.404b7ba9.png"
+  },
+  {
+    "revision": "2ec2ff0c5360ef0b027ae28076b411e3",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/design_02.2ec2ff0c.png"
+  },
+  {
+    "revision": "f8f640a2729edd74987e64eb52b45b6c",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/din-next-lt-w23-light.f8f640a2.ttf"
+  },
+  {
+    "revision": "799cc3b49fa4cadc660b7b6303aea517",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/din-next-lt-w23-medium.799cc3b4.ttf"
+  },
+  {
+    "revision": "330c17262cfca9e8e8c39ebc57282d3e",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/din-next-lt-w23-regular.330c1726.ttf"
+  },
+  {
+    "revision": "33917f287c4e05446e816d39a0b4a966",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/din-next-lt-w23-ultra-light.33917f28.ttf"
+  },
+  {
+    "revision": "7499f2d02b516cf1fc4f9bd7b2034433",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/feedback.7499f2d0.svg"
+  },
+  {
+    "revision": "bc12ee06982e8bb67405604d9305595b",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/feedback_left_bottom.bc12ee06.png"
+  },
+  {
+    "revision": "dcbc76fab3b23c8a343095afec9f9a04",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/feedback_left_middle.dcbc76fa.png"
+  },
+  {
+    "revision": "cfc3e6b9ad5c934d7170353b1df1f9fc",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/feedback_middle_bottom.cfc3e6b9.png"
+  },
+  {
+    "revision": "eac719f6c0e2f3cb7cab66a2fe98ac18",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/feedback_right_middle.eac719f6.png"
+  },
+  {
+    "revision": "bca2681babfd80a4db54f1821d743e25",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/feedback_right_top.bca2681b.png"
+  },
+  {
+    "revision": "32bd57ebc8bb5326750147674e961a63",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/footer-logo.32bd57eb.svg"
+  },
+  {
+    "revision": "acc6189ff32e93b2aac612e704626405",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/new_shipment.acc6189f.svg"
+  },
+  {
+    "revision": "40fc6e564b67b869f00607a04ea3631d",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/orange_circle.40fc6e56.svg"
+  },
+  {
+    "revision": "411178f2d4e8847a08198222079f7a50",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/order_3.411178f2.svg"
+  },
+  {
+    "revision": "25e722912c7009dfe5a9171b3ff13584",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/plus.25e72291.svg"
+  },
+  {
+    "revision": "5bcdd6a5cf25ac73ae034c896eabd310",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/red_circle.5bcdd6a5.svg"
+  },
+  {
+    "revision": "9b0ff11cd774219df895c988f8b6a1cd",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/right_mark.9b0ff11c.svg"
+  },
+  {
+    "revision": "c65b46671a385d81e683300dc3b274b7",
+    "url": "/Maysan-For-Sale-Omar.dev/static/media/signature.c65b4667.svg"
+  }
+]);
